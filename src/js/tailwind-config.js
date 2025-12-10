1cf5e44202d1fd17f00fb590d9d036975fb677b3
Luana Mennuti <luigina.mennuti2@studio.unibo.it>
2025-12-10 16:32:53 +0100

    chore: setup tailwind configuration

---
+++ src/js/tailwind-config.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                spotBg: '#000000',
+                spotPaper: '#F5F2EB',
+                spotPrimary: '#16A34A',
+                toolbarText: '#1F1F1F'
+            }
+        }
+    }
+}
