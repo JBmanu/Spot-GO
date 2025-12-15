6ac1d8680a8a2b1e40446ee7d3aa752bc92027aa
Luana Mennuti <luigina.mennuti2@studio.unibo.it>
2025-12-15 17:31:42 +0100

    chore: update tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,19 +7,6 @@ import containerQueries from "@tailwindcss/container-queries";
 export default {
     content: ["./src/**/*.{html,js}"],
 
-    // theme: {
-    //     extend: {
-    //         colors: {
-    //
-    //             // "main-paper": "var(--color-main_paper)",
-    //             "text-color": "var(--color-text_color)",
-    //             "primary-color": "var(--color-primary_color)",
-    //             "secondary-color": "var(--color-secondary_color)",
-    //             "accent-color": "var(--color-accent_color)"
-    //         }
-    //     }
-    // },
-
     plugins: [
         forms,
         typography,
