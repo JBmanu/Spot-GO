0dee577895fb90709f8e51fb03e3cac83e77647f
Manuel Buizo <buizomanuel@gmail.com>
2026-01-04 01:39:28 +0100

    feat: completed animation of missions type bar

--- src/js/goals/goals.js
+++ src/js/goals/goals.js
@@ -1,51 +1,7 @@
-﻿
+﻿import {initializeMissionsBar} from "./missionsTypeBar.js";
 
+export async function initializeGoals() {
+    await initializeMissionsBar();
 
-
-
-export function initializeGoals() {
-    const buttons = document.querySelectorAll(".mission-type-btn");
-    const indicator = document.getElementById("indicator");
-    const container = document.getElementById("tab-container");
-
-    if (!buttons.length || !indicator || !container) return;
-
-    let activeIndex = 0;
-
-    const updateIndicator = (index) => {
-        const button = buttons[index];
-
-        // Prendi il rect del bottone e del contenitore
-        const buttonRect = button.getBoundingClientRect();
-        const containerRect = container.getBoundingClientRect();
-
-        // Posizione relativa al contenitore
-        const left = buttonRect.left - containerRect.left - 1;
-
-        // Applica dimensioni e posizione
-        indicator.style.width = `${buttonRect.width}px`;
-        indicator.style.height = `${buttonRect.height}px`;
-        indicator.style.left = `${left}px`;
-    };
-
-    // Inizializza indicatore sul primo bottone
-    updateIndicator(activeIndex);
-
-    // Aggiorna indicatore al click sui bottoni
-    buttons.forEach((btn, index) => {
-        btn.addEventListener("click", () => {
-            activeIndex = index;
-            updateIndicator(index);
-
-            // Gestione classe active
-            buttons.forEach(b => b.classList.remove("active"));
-            btn.classList.add("active");
-        });
-    });
-
-    // Aggiorna indicatore al resize della finestra
-    window.addEventListener("resize", () => updateIndicator(activeIndex));
-
-    // Classe active al bottone di default
-    buttons[activeIndex].classList.add("active");
+    console.log("Goals module initialized");
 }
---
+++ src/js/goals/missionsTypeBar.js
@@ -0,0 +1,82 @@
+﻿const CONTAINER = "tab-bar";
+const INDICATOR = "tab-indicator";
+const BUTTON = ".mission-type-btn";
+
+function activeButton(buttons, activeButton) {
+    const ICON_ACTIVE_CLASS = "icon";
+    const ICON_INACTIVE_CLASS = "icon-mini";
+
+    const TEXT_ACTIVE_CLASS = "font-semibold";
+    const TEXT_INACTIVE_CLASS = "font-normal";
+
+    buttons.forEach(button => {
+        const icon = button.querySelector("img");
+        const text = button.querySelector("span");
+
+        // reset stato
+        icon.classList.remove(ICON_ACTIVE_CLASS);
+        icon.classList.add(ICON_INACTIVE_CLASS);
+        text.classList.remove(TEXT_ACTIVE_CLASS);
+        text.classList.add(TEXT_INACTIVE_CLASS);
+    });
+
+    // stato attivo
+    const activeIcon = activeButton.querySelector("img");
+    const activeText = activeButton.querySelector("span");
+
+    activeIcon.classList.remove(ICON_INACTIVE_CLASS);
+    activeIcon.classList.add(ICON_ACTIVE_CLASS);
+    activeText.classList.remove(TEXT_INACTIVE_CLASS);
+    activeText.classList.add(TEXT_ACTIVE_CLASS);
+}
+
+function updateIndicatorPosition(container, indicator, button) {
+    // Prendi il rect del bottone e del contenitore
+    const buttonRect = button.getBoundingClientRect();
+    const containerRect = container.getBoundingClientRect();
+
+    // Posizione relativa al contenitore
+    const left = buttonRect.left - containerRect.left - 1;
+    // Applica dimensioni e posizione
+    indicator.style.width = `${buttonRect.width}px`;
+    indicator.style.height = `${buttonRect.height}px`;
+    indicator.style.left = `${left}px`;
+}
+
+
+export async function initializeMissionsBar() {
+    const container = document.getElementById(CONTAINER);
+    const indicator = document.getElementById(INDICATOR);
+    const buttons = document.querySelectorAll(BUTTON);
+
+    if (!buttons.length || !indicator || !container) return;
+    let activeIndex = 0;
+
+    // Inizializza indicatore sul primo bottone
+    activeButton(buttons, buttons[activeIndex])
+    buttons.forEach(btn => {
+        btn.addEventListener("transitionend", () => {
+            updateIndicatorPosition(container, indicator, buttons[activeIndex]);
+        });
+    })
+    updateIndicatorPosition(container, indicator, buttons[activeIndex]);
+
+    // Aggiorna indicatore al click sui bottoni
+    buttons.forEach((btn, index) => {
+        btn.addEventListener("click", () => {
+            activeIndex = index;
+            activeButton(buttons, buttons[index]);
+            updateIndicatorPosition(container, indicator, buttons[activeIndex]);
+            // Gestione classe active
+            buttons.forEach(b => b.classList.remove("active"));
+            btn.classList.add("active");
+        });
+    });
+
+    // Aggiorna indicatore al resize della finestra
+    window.addEventListener("resize", () => updateIndicatorPosition(container, indicator, buttons[activeIndex]));
+
+    // Classe active al bottone di default
+    buttons[activeIndex].classList.add("active");
+    console.log("Missions Type Bar initialized");
+}
