5c87da9e32d98cfd781e4ba1f12bcfa16910afcc
Luana Mennuti <luigina.mennuti2@studio.unibo.it>
2026-01-13 00:00:21 +0100

    refactor: update path references

--- src/js/paths.js
+++ src/js/paths.js
@@ -1,20 +1,21 @@
 export const PATHS = {
     html: {
-        homepage: "./html/homepage.html",
-        map: "./html/map.html",
-        community: "./html/community.html",
-        goals: "./html/goals.html",
-        profile: "./html/profile.html",
-        login: "./html/login.html",
+        homepage: "../html/homepage.html",
+        map: "../html/map.html",
+        community: "../html/community.html",
+        goals: "../html/goals.html",
+        profile: "../html/profile.html",
+        login: "../html/login.html",
 
         header: "../html/common-pages/header.html",
         toolbar: "../html/common-pages/toolbar.html",
+        spotTemplates: "../html/common-pages/spot-templates.html",
 
-        homepageSaved: "./html/homepage-pages/saved-spots-section.html",
-        homepageNearby: "./html/homepage-pages/nearby-spots-section.html",
-        homepageTopRated: "./html/homepage-pages/toprated-carousel-section.html",
+        homepageSaved: "../html/homepage-pages/saved-spots-section.html",
+        homepageNearby: "../html/homepage-pages/nearby-spots-section.html",
+        homepageTopRated: "../html/homepage-pages/toprated-spots-section.html",
 
-        viewAllSaved: "./html/homepage-pages/view-all/view-all-saved.html",
-        profileOverview: "./html/profile-pages/overview.html",
+        viewAllSaved: "../html/homepage-pages/view-all/view-all-saved.html",
+        profileOverview: "../html/profile-pages/overview.html",
     },
 };
