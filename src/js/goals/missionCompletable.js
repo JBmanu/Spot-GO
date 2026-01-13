100c6d8dec2c8e5f22fb951f65c092e4cf015779
Manuel Buizo <buizomanuel@gmail.com>
2026-01-13 12:06:13 +0100

    feat: active completed when click

--- src/js/goals/missionCompletable.js
+++ src/js/goals/missionCompletable.js
@@ -1,4 +1,18 @@
 export async function initializeCompletable() {
+    let spotMissions = document.querySelectorAll('.spot-mission');
+    let completableOverlay = document.querySelectorAll('.spot-mission-complete');
+
+    completableOverlay.forEach(overlay => overlay.classList.add('inactive'))
+
+    spotMissions.forEach(btn => {
+        btn.addEventListener('click', () => {
+            const completableOverlay = btn.querySelector('.spot-mission-complete');
+            completableOverlay.classList.add('inactive');
+
+            // auto-hide dopo 1.8s (come iOS toast)
+            setTimeout(() => completableOverlay.classList.remove('inactive'), 1800);
+        });
+    });
 
     console.log("Completable initialized")
 }
