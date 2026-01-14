591afbfe3f93eda113d2d5ecc0709cab166d56d8
Manuel Buizo <buizomanuel@gmail.com>
2026-01-14 11:07:47 +0100

    feat: create complete div from javascript

--- src/js/goals/goals.js
+++ src/js/goals/goals.js
@@ -3,7 +3,11 @@ import {initializeSpotsMissions} from "./spotsMissions.js";
 import {initializeMissions} from "./missions.js";
 import {initializeCompletable} from "./missionCompletable.js";
 
+let isInitialized = false;
+
 export async function initializeGoals() {
+    if (isInitialized) return;
+    isInitialized = true;
     await initializeSpotsMissions();
     await initializeMissionsBar();
     await initializeMissions();
--- src/js/goals/missionCompletable.js
+++ src/js/goals/missionCompletable.js
@@ -1,18 +1,37 @@
 export async function initializeCompletable() {
-    let spotMissions = document.querySelectorAll('.spot-mission');
-    let completableOverlay = document.querySelectorAll('.spot-mission-complete');
+    // let spotMissions = document.querySelectorAll('.spot-mission');
+    // let completableOverlay = document.querySelectorAll('.spot-mission-complete');
+    //
+    // completableOverlay.forEach(overlay => overlay.classList.add('inactive'))
+    //
+    // spotMissions.forEach(btn => {
+    //     btn.addEventListener('click', () => {
+    //         const completableOverlay = btn.querySelector('.spot-mission-complete');
+    //         completableOverlay.classList.toggle('inactive');
+    //     });
+    // });
 
-    completableOverlay.forEach(overlay => overlay.classList.add('inactive'))
+    const completableMissions = document.querySelectorAll('.completable');
+    completableMissions.forEach(mission => {
+        const completeDiv = mission.querySelector('.spot-mission-complete');
+        if (completeDiv) return;
 
-    spotMissions.forEach(btn => {
-        btn.addEventListener('click', () => {
-            const completableOverlay = btn.querySelector('.spot-mission-complete');
-            completableOverlay.classList.toggle('inactive');
+        mission.innerHTML += `
+            <!-- Complete -->
+            <div class="center-ctn spot-mission-complete inactive">
+                <span class="text-lg font-bold text-green-700">COMPLETATO ✔</span>
+            </div>
+        `;
+        // const box = document.createElement('div');
+        // box.className = `center-ctn spot-mission-complete inactive`;
+        // box.innerHTML = `<span class="text-lg font-bold text-green-700">COMPLETATO ✔</span>`;
+        // mission.appendChild(box);
 
-            // auto-hide dopo 1.8s (come iOS toast)
-            // setTimeout(() => completableOverlay.classList.remove('inactive'), 1800);
-        });
-    });
+        mission.addEventListener('click', () => {
+            mission.querySelector('.spot-mission-complete').classList.toggle('inactive');
+        })
+    })
+
 
     console.log("Completable initialized")
 }
