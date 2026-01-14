3dc45561252039720d9da00ced80bdd81abeabca
Manuel Buizo <buizomanuel@gmail.com>
2026-01-14 12:36:27 +0100

    feat: generate theme missions

--- src/js/goals/GenerateMissions.js
+++ src/js/goals/GenerateMissions.js
@@ -18,6 +18,14 @@ export async function generateMissions() {
             i,
             2)
     }
+    for (let i = 0; i < 3; i++) {
+        generateThemeMissions(
+            "Daily Mission",
+            "Complete daily activities",
+            50,
+            i,
+            3)
+    }
 }
 
 // Generate spots
@@ -92,6 +100,27 @@ function generateSpotMissions(title, description, exp) {
 function generateDailyMissions(title, description, exp, progress, allProgress) {
     const dailyCtn = document.querySelectorAll('.missions-card');
     dailyCtn[0].innerHTML +=
+        `<div class="between-ctn glass-strong interactive completable px-5 py-4">
+            <!-- Lato sinistro -->
+            <div class="space-y-1">
+                <h3 class="text-sm font-semibold text-gray-800">${title}</h3>
+                <p class="text-xs text-gray-600">${description}</p>
+                <div class="flex items-center gap-1 text-xs text-gray-700">
+                    <img src="../assets/icons/goals/FlashOn.svg" class="w-4 h-4" alt="Reward Icon"/>
+                    <span>+${exp} XP</span>
+                </div>
+            </div>
+            <!-- Lato destro -->
+            <div class="center-ctn">
+                <span class="text-lg font-medium text-gray-500">${progress} / ${allProgress}</span>
+            </div>
+        </div>`;
+}
+
+// Generate Theme missions
+function generateThemeMissions(title, description, exp, progress, allProgress) {
+    const themeCtn = document.querySelectorAll('.missions-card');
+    themeCtn[1].innerHTML +=
         `<div class="between-ctn glass-strong interactive completable px-5 py-4">
             <!-- Lato sinistro -->
             <div class="space-y-1">
