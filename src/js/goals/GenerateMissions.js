b39958f6c018162f44d81cb20c536e9030a5b31f
Manuel Buizo <buizomanuel@gmail.com>
2026-01-14 12:34:44 +0100

    feat: generate daily missions

--- src/js/goals/GenerateMissions.js
+++ src/js/goals/GenerateMissions.js
@@ -9,6 +9,15 @@ export async function generateMissions() {
             "Usa quella fotocamera del cellulare, ora!",
             100)
     }
+
+    for (let i = 0; i < 2; i++) {
+        generateDailyMissions(
+            "Daily Mission",
+            "Complete daily activities",
+            50,
+            i,
+            2)
+    }
 }
 
 // Generate spots
@@ -77,4 +86,25 @@ function generateSpotMissions(title, description, exp) {
                 </div>
             </button>`;
     })
+}
+
+// Generate Daily missions
+function generateDailyMissions(title, description, exp, progress, allProgress) {
+    const dailyCtn = document.querySelectorAll('.missions-card');
+    dailyCtn[0].innerHTML +=
+        `<div class="between-ctn glass-strong interactive completable px-5 py-4">
+            <!-- Lato sinistro -->
+            <div class="space-y-1">
+                <h3 class="text-sm font-semibold text-gray-800">${title}</h3>
+                <p class="text-xs text-gray-600">${description}</p>
+                <div class="flex items-center gap-1 text-xs text-gray-700">
+                    <img src="../assets/icons/goals/FlashOn.svg" class="w-4 h-4" alt="Reward Icon"/>
+                    <span>+${exp} XP</span>
+                </div>
+            </div>
+            <!-- Lato destro -->
+            <div class="center-ctn">
+                <span class="text-lg font-medium text-gray-500">${progress} / ${allProgress}</span>
+            </div>
+        </div>`;
 }
