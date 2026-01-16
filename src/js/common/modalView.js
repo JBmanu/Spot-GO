c9970af010a9ea1d62a8220da9cf81f97082f250
Matteo <matteo.violani@studio.unibo.it>
2026-01-16 18:29:29 +0100

    refactor: modalView function to open in modal some html page

---
+++ src/js/common/modalView.js
@@ -0,0 +1,58 @@
+
+let modalElement = null;
+let isModalOpen = false;
+
+export async function openModal(htmlPath, parentSelector, initPageContent) {
+    if (isModalOpen) return;
+
+    const response = await fetch(htmlPath);
+    if (!response.ok) return;
+
+    const html = await response.text();
+    const tempDiv = document.createElement("div");
+    tempDiv.innerHTML = html;
+    modalElement = tempDiv.firstElementChild;
+
+    const parentNode = document.querySelector(parentSelector);
+    if (!parentNode) return;
+    parentNode.appendChild(modalElement);
+
+    initPageContent(modalElement);
+
+    isModalOpen = true;
+    modalElement.style.display = "flex";
+    requestAnimationFrame(() => {
+        modalElement.classList.add("active");
+    });
+
+    const mainContainer = document.getElementById("main");
+    if (mainContainer) {
+        mainContainer.style.overflow = "hidden";
+    }
+    return modalElement;
+}
+
+/**
+ * Close modal view after execution of onCloseAction
+ * @param {Async function with action to do before closing modal modalElement => void} onCloseAction 
+ * @returns nothing
+ */
+export async function closeModal(onCloseAction) {
+    if (!isModalOpen || !modalElement) return;
+
+    isModalOpen = false;
+    modalElement.classList.remove("active");
+
+    setTimeout(async () => {
+        modalElement.style.display = "none";
+        if (onCloseAction) {
+            await onCloseAction(modalElement);
+        }
+        const mainContainer = document.getElementById("main");
+        if (mainContainer) {
+            mainContainer.style.overflow = "";
+        }
+        modalElement.remove();
+        modalElement = null;
+    }, 300);
+}
