ea2920ea150cc7bd35b7da703077144757db13b8
Matteo <matteo.violani@studio.unibo.it>
2026-01-26 22:21:57 +0100

    fix: show modals prompt full screen, not hide dinamic island

--- src/js/ui/confirmModal.js
+++ src/js/ui/confirmModal.js
@@ -1,30 +1,30 @@
 import { openModal, closeModal } from "../common/modalView.js";
 
-export function showConfirmModal(title = "", message = "") {
+export function showConfirmModal(title = "", message = "", parentSelector = ".phone-screen") {
     return new Promise(async (resolve) => {
-        const modalElement = await openModal("../html/common-components/confirmation-modal.html", ".phone-screen", () => {});
-        
-        modalElement.querySelector("#confirmation-modal-title").innerHTML = title;
-        modalElement.querySelector("#modal-description").innerHTML = message;
-        const closeButton = modalElement.querySelector("#close-remove-btn");
-        const confirmButton = modalElement.querySelector("#confirm-remove-btn");
-        const backdrop = document.querySelector(".background-modal-overlay");
-        
-        closeButton.addEventListener('click', () => {
-            closeModal();
-            resolve(false);
-        });
-
-        confirmButton.addEventListener('click', async () => {
-            closeModal();
-            resolve(true);
-        });
-
-        backdrop.addEventListener('click', (e) => {
-            if (e.target === backdrop) {
+        await openModal("../html/common-components/confirmation-modal.html", parentSelector, (modal) => {
+            modal.querySelector("#confirmation-modal-title").innerHTML = title;
+            modal.querySelector("#modal-description").innerHTML = message;
+            const closeButton = modal.querySelector("#close-remove-btn");
+            const confirmButton = modal.querySelector("#confirm-remove-btn");
+            const backdrop = document.querySelector(".background-modal-overlay");
+            
+            closeButton.addEventListener('click', () => {
                 closeModal();
                 resolve(false);
-            }
+            });
+
+            confirmButton.addEventListener('click', async () => {
+                closeModal();
+                resolve(true);
+            });
+
+            backdrop.addEventListener('click', (e) => {
+                if (e.target === backdrop) {
+                    closeModal();
+                    resolve(false);
+                }
+            });
         });
     });
 }
