d2d16fc9a1960ea5664d67eeddeed14b10bcdcd7
Manuel Buizo <buizomanuel@gmail.com>
2026-02-13 13:23:55 +0100

    feat: remove clickable

--- src/js/goals/interaction/missionCompletable.js
+++ src/js/goals/interaction/missionCompletable.js
@@ -1,20 +1,20 @@
 export async function initializeCompletable() {
-    const missions = document.querySelectorAll('.completable')
+    // const missions = document.querySelectorAll('.completable')
 
-    missions.forEach(spotMissions => {
-        const titleSpan = spotMissions.querySelector('.mission-title');
-        if (!titleSpan) return;
-
-        spotMissions.addEventListener('click', () => {
-            console.log( "Clicked mission completable")
-            if (titleSpan.classList.contains('completed')) {
-                markMissionAsUncompleted(spotMissions)
-            } else {
-                markMissionAsCompleted(spotMissions)
-            }
-        })
-    })
-    console.log("Completable initialized")
+    // missions.forEach(spotMissions => {
+    //     const titleSpan = spotMissions.querySelector('.mission-title');
+    //     if (!titleSpan) return;
+    //
+    //     spotMissions.addEventListener('click', () => {
+    //         console.log( "Clicked mission completable")
+    //         if (titleSpan.classList.contains('completed')) {
+    //             markMissionAsUncompleted(spotMissions)
+    //         } else {
+    //             markMissionAsCompleted(spotMissions)
+    //         }
+    //     })
+    // })
+    // console.log("Completable initialized")
 }
 
 export function markMissionAsCompleted(missionEl) {
